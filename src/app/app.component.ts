b9d9074179c3e5b17078748aa750554ad6b4fc39
rootn3rd <krishnahare14@gmail.com>
2017-11-13 00:29:42 +0530

    Basic firebase integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/take';
+
+import * as firebase from 'firebase/app';
+import { PACKAGE_ROOT_URL } from '@angular/core/src/application_tokens';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,107 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app-reciator';
+
+  // private itemDoc: AngularFirestoreDocument<any>;
+  // item: Observable<any>;
+
+  // private itemCollection: AngularFirestoreCollection<any>;
+  // items: Observable<any[]>;
+
+  private userCollection: AngularFirestoreCollection<any>;
+  allUsers$: Observable<any>;
+  allUserValues: any[];
+  otherUsers: any[] = [];
+  currentLoggedInUser: Observable<any>;
+
+  private userInfo: any;
+  currentUserRefId: any = null;
+  currentUserDoc: AngularFirestoreDocument<any>;
+
+  constructor(private db: AngularFirestore, private afAuth: AngularFireAuth) {
+
+    this.userCollection = db.collection<any>('users');
+    this.allUsers$ = this.userCollection.snapshotChanges().map(s => {
+      console.log('User snapshot changes', s);
+      return s.map(p => {
+        let cur = p.payload.doc.data();
+        let id = p.payload.doc.id;
+        return { id, ...cur };
+      });
+    });
+
+
+    this.allUsers$.subscribe(x => {
+      console.log(x);
+      this.allUserValues = x;
+
+      if (this.currentUserRefId) {
+        this.otherUsers = x.filter(user => user.id !== this.currentUserRefId);
+      }
+    })
+
+  }
+
+  appreciate(user) {
+    this.db.collection('users').doc(user.id).update({
+      appreciationCounter: user.appreciationCounter + 1
+    });
+  }
+
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(s => {
+        this.userInfo = s.additionalUserInfo;
+        console.log(s);
+
+        if (this.userInfo.isNewUser) {
+          //add to the collection
+          this.userCollection.add({
+            uid: this.userInfo.profile.id,
+            name: this.userInfo.profile.name,
+            appreciationCounter: 0,
+            appreciated: [],
+            appreciatedBy: []
+          }).then(ref => {
+            this.currentUserRefId = ref.id;
+            console.log(this.currentUserRefId);
+            this.currentLoggedInUser = this.db.doc('users/' + this.currentUserRefId).valueChanges();
+            this.otherUsers = this.allUserValues.filter(x => x.id !== this.currentUserRefId);
+
+          });
+        } else {
+          let curr = this.allUserValues.find(p => p.uid === this.userInfo.profile.id);
+          if (curr) {
+            this.currentUserRefId = curr.id;
+            console.log(this.currentUserRefId);
+
+            this.currentLoggedInUser = this.db.doc('users/' + this.currentUserRefId).valueChanges();
+            this.otherUsers = this.allUserValues.filter(x => x.id !== this.currentUserRefId);
+
+          }
+
+        }
+
+
+      }).catch(e => {
+        console.log('Something went wrong!', e);
+      });
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  // addTestUsers() {
+  //   for (var i = 0; i < 5; i++) {
+  //     this.userCollection.add({
+  //       uid: Math.random() * 100,
+  //       name: 'Test ' + i,
+  //       appreciationCounter: Math.random() * 20,
+  //       appreciated: [],
+  //       appreciatedBy: []
+  //     });
+  //   }
+  // }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDcft8__giDVI2QMssAyuYnf0_OzKF4m_E",
+    authDomain: "appreciator-1489.firebaseapp.com",
+    databaseURL: "https://appreciator-1489.firebaseio.com",
+    projectId: "appreciator-1489",
+    storageBucket: "",
+    messagingSenderId: "372238930856"
+  }
 };
